b091bb85cac24d6f7231ed78a64f68f066120dea
Berniece Buttermilk <bbutterm@at-q6.21-school.ru>
2020-02-06 20:51:06 +0300

    das

--- validation.c
+++ validation.c
@@ -12,8 +12,7 @@
 
 #include "push_swap.h"
 
-
-int	atoi_for_ps(const char *str)
+int		atoi_for_ps(const char *str)
 {
 	int					i;
 	long				nbr;
@@ -23,7 +22,7 @@ int	atoi_for_ps(const char *str)
 	nbr = 0;
 	negative = 1;
 	while ((str[i] == '\n') || (str[i] == '\t') || (str[i] == '\v') ||
-		   (str[i] == ' ') || (str[i] == '\f') || (str[i] == '\r'))
+		(str[i] == ' ') || (str[i] == '\f') || (str[i] == '\r'))
 		i++;
 	(str[i] == '-') ? negative *= -1 : negative;
 	if (str[i] == '-' || str[i] == '+')
@@ -37,21 +36,21 @@ int	atoi_for_ps(const char *str)
 	if (nbr > 2147483647 || nbr < -2147483648)
 	{
 		write(1, "Error\n", 6);
-		exit (1);
+		exit(1);
 	}
 	return ((int)nbr);
 }
 
-void    check_double(char *str, t_lis *p)
+void		check_double(char *str, t_lis *p)
 {
 	int		i;
-	int     j;
+	int		j;
 
 	p->argcount = len_nbr(str);
-	init(p,p->argcount);
+	init(p, p->argcount);
 	i = 0;
 	j = 0;
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		while ((str[i] == '\n') || (str[i] == '\t') || (str[i] == '\v') || \
 				(str[i] == ' ') || (str[i] == '\f') || (str[i] == '\r'))
@@ -62,27 +61,26 @@ void    check_double(char *str, t_lis *p)
 			p->v[j] = atoi_for_ps(&str[i]);
 			j++;
 		}
-		while(ft_isdigit(str[i]) || str[i] == '-')
+		while (ft_isdigit(str[i]) || str[i] == '-')
 			i++;
 	}
-	ssort(p->v,p->argcount);
+	ssort(p->v, p->argcount);
 	p->min = p->v[0];
 	p->max = p->v[p->argcount - 1];
 	p->mid = p->v[p->argcount - 1 / 2];
-
 	i = 0;
-	while(i < p->argcount - 1)
+	while (i < p->argcount - 1)
 	{
 		if (p->v[i] == p->v[i + 1])
 		{
 			write(1, "Error\n", 6);
-			exit (1);
+			exit(1);
 		}
 		i++;
 	}
 }
 
-int		validelemet(char *stack1)
+int			validelemet(char *stack1)
 {
 	int	i;
 
@@ -91,37 +89,35 @@ int		validelemet(char *stack1)
 	{
 		if (stack1[i] == '-' && ft_isdigit(stack1[i + 1]))
 			i++;
-		if((stack1[i] >= 48 && stack1[i] <= 57) || stack1[i] == ' ')
+		if ((stack1[i] >= 48 && stack1[i] <= 57) || stack1[i] == ' ')
 			i++;
 		else
-			return(0);
+			return (0);
 	}
-	return(1);
+	return (1);
 }
 
-void    validate_of_stack(char *str, t_lis *p)
+void		validate_of_stack(char *str, t_lis *p)
 {
-	int     i;
+	int		i;
 
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
 		while ((str[i] == '\n') || (str[i] == '\t') || (str[i] == '\v') || \
 				(str[i] == ' ') || (str[i] == '\f') || (str[i] == '\r'))
 			i++;
 		if (str[i] != '\0')
 			atoi_for_ps(&str[i]);
-		while(ft_isdigit(str[i]) || str[i] == '-')
+		while (ft_isdigit(str[i]) || str[i] == '-')
 		{
-			if(ft_isdigit(str[i]) && str[i + 1] == '-')
+			if (ft_isdigit(str[i]) && str[i + 1] == '-')
 			{
 				write(1, "Error\n", 6);
-				exit (1);
+				exit(1);
 			}
 			i++;
 		}
 	}
 	check_double(str, p);
 }
-
-
